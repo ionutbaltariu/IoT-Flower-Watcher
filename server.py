24220f47db46c66a69e8a0543f7bcee7e3339524
xeno-john <ionut-alexandru.baltariu@student.tuiasi.ro>
2021-06-12 20:41:40 +0300

    reformat code

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ from email.mime.text import MIMEText
 
 app = Flask(__name__)
 
-last_soil_humidity_value = 0
+last_soil_moisture_value = 0
 last_temperature_value = 0
 last_air_humidity_value = 0
 last_sent_email_hour = 0
@@ -24,50 +24,45 @@ email = 'iotflowermonitoring@gmail.com'
 email_password = str(os.environ.get("iot_password"))
 mutex = threading.Lock()
 
-def send_mail(message):
-    server=smtplib.SMTP('smtp.gmail.com',587)
-    server.starttls()
-    server.login(email,email_password)
-                    
-
-    email_content = MIMEText(message,'plain')
-    email_content['Subject']= 'Flower Monitoring Report'
-
-    server.sendmail(email,"ionut.alexandru.baltariu@gmail.com",email_content.as_string())
-    server.quit()
-
-    print("Sent mail!")
 
 @app.route('/')
 def index():
-    return render_template('index.html', soil_humidity=str(last_soil_humidity_value), temperature=str(last_temperature_value),
-            air_humidity=str(last_air_humidity_value))
+    return render_template('index.html', soil_humidity=str(last_soil_moisture_value),
+                           temperature=str(last_temperature_value),
+                           air_humidity=str(last_air_humidity_value))
 
-@app.route('/',methods=['POST']) 
+
+@app.route('/', methods=['POST'])
 def on_play():
     setup()
     play(final_countdown_melody, final_countdown_tempo, 0.30, 1.2000)
     return index()
 
+
 def get_values_from_serial():
-    global last_soil_humidity_value
+    global last_soil_moisture_value
     global last_sent_email_hour
     global email_password
-    ser = serial.Serial('/dev/ttyACM0',9600, timeout=1)
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     while True:
         try:
-            read_serial=ser.readline()
+            read_serial = ser.readline()
             read_json = json.loads(read_serial.decode())
-            if(read_json['moisture'] is not None):
-                last_soil_humidity_value = float(read_json['moisture'])*100
-                if last_soil_humidity_value < 50 and (abs(last_sent_email_hour - datetime.datetime.now().hour)) >= 1:
-                    last_sent_email_hour = datetime.datetime.now().hour
-                    msg = "Your flower has low soil moisture (" + str(last_soil_humidity_value) + ")! Water it urgently!"
-                    mutex.acquire()
-                    send_mail(msg)
-                    mutex.release()
+            if read_json['moisture'] is not None:
+                last_soil_moisture_value = float(read_json['moisture']) * 100
+                if abs(last_sent_email_hour - datetime.datetime.now().hour) >= 1:
+                    if last_soil_moisture_value < 50:
+                        last_sent_email_hour = datetime.datetime.now().hour
+                        msg = "Your flower has low soil moisture (" + str(
+                            last_soil_moisture_value) + "%)! Water it urgently!"
+                        send_mail(msg)
+                    elif last_soil_moisture_value > 80:
+                        last_sent_email_hour = datetime.datetime.now().hour
+                        msg = "Your flower has high soil moisture (" + str(
+                            last_soil_moisture_value) + "%)! Stop watering!"
+                        send_mail(msg)
 
-            print(last_soil_humidity_value)
+            print(last_soil_moisture_value)
         except json.decoder.JSONDecodeError:
             pass
         except serial.serialutil.SerialException:
@@ -78,7 +73,6 @@ def read_temperature_and_humidity():
     global last_air_humidity_value
     global last_temperature_value
     global last_sent_email_hour_th
-    global mutex
     while True:
         air_humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
         if air_humidity is None and temperature is None:
@@ -86,13 +80,33 @@ def read_temperature_and_humidity():
         else:
             last_air_humidity_value = air_humidity
             last_temperature_value = temperature
-            if (temperature < 18 or temperature >= 30 or air_humidity < 25 or air_humidity > 50) and abs(last_sent_email_hour_th - datetime.datetime.now().hour) >= 1: 
-                msg = "Temperature/air humidity in flower room too low/high:" + str(temperature) + "*C and " + str(air_humidity) + "%"
+            if (temperature < 18 or temperature >= 30 or air_humidity < 25 or air_humidity > 50) and \
+                    abs(last_sent_email_hour_th - datetime.datetime.now().hour) >= 1:
+                msg = "Temperature/air humidity in flower room too low/high:" + str(temperature) + "*C and " + str(
+                    air_humidity) + "%"
                 last_sent_email_hour_th = datetime.datetime.now().hour
-                mutex.acquire()
                 send_mail(msg)
-                mutex.release()
         time.sleep(3)
+
+
+def send_mail(message):
+    global mutex
+
+    mutex.acquire()
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email, email_password)
+
+    email_content = MIMEText(message, 'plain')
+    email_content['Subject'] = 'Flower Monitoring Report'
+
+    server.sendmail(email, "ionut.alexandru.baltariu@gmail.com", email_content.as_string())
+    server.quit()
+
+    print("Sent mail!")
+
+    mutex.release()
 
 
 moisture_thread = threading.Thread(target=get_values_from_serial)
